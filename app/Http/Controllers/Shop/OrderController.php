7c07653d2fc7889e6814d83fd626a1ef2f13b77e
Ali hamza <ah8483120@gmail.com>
2022-09-12 21:57:21 +0500

    new

--- app/Http/Controllers/Shop/OrderController.php
+++ app/Http/Controllers/Shop/OrderController.php
@@ -25,13 +25,12 @@ class OrderController extends Controller
     public function changeStatus($id,$status){
     $myshop=shop::where('user_id',auth()->user()->id)->first();
     $product=ShopProduct::where('shop_id',$myshop->id)->get();
-        $order=Order::find($id);
-        foreach($product as $p){
-    $shop=OrderProduct::where('order_id',$order->id)->where('product_id',$p->id)->first();
-    
-      
+    $order=Order::find($id);
+    foreach($product as $p){
+        $shop=OrderProduct::where('order_id',$order->id)->where('product_id',$p->id)->first();
+        if($shop){
             $shop->update(['status'=>$status,'status_change_date'=>now()]);
-       
+        }   
     }
         return redirect()->route('shop.order.detail',[$order->id]);
     }
